1e8a2d4f41ab063f5a7fd94a3f01ac84c7175ca8
LukeJurat <jf2ma@virginia.edu>
2018-03-27 15:55:31 -0400

    ya boi

--- submissions/01-r4ds-data-transformation-Jurat-Luke.R
+++ submissions/01-r4ds-data-transformation-Jurat-Luke.R
@@ -1,1 +1,17 @@
-library(ggplot2)
+library(tidyverse)
+library(nycflights13)
+?flights
+
+#Question 1 
+
+flights %>%
+filter(dest == 'LAX')%>%
+nrow()
+
+flights %>%
+filter(origin == 'LAX')%>%
+nrow()
+
+flights %>%
+filter(distance >= 2000)%>%
+nrow()
