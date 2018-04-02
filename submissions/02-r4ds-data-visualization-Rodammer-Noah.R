2a83c1215872413c1f8e9fb9f62d05d60d97f803
noahrodammer <noahrodammer@gmail.com>
2018-04-02 11:42:41 -0400

    Richard's HW

--- submissions/02-r4ds-data-visualization-Rodammer-Noah.R
+++ submissions/02-r4ds-data-visualization-Rodammer-Noah.R
@@ -1,51 +1,25 @@
-#############
-# FILE INFO #
-#############
+#' ---
+#' title: "Answers to the R4DS Data Visualization Exercise"
+#' author: Noah Rodammer
+#' date: April 2, 2018
+#' output: github_document
+#' ---
 
-# Title: GCOMM 7140 Homework 2
-# Author: Noah Rodammer
-
-
-#################
-# LOAD PACKAGES #
-#################
 
 library(tidyverse)
-library(dplyr)
-library(devtools)
-library(ggplot2)
-
 ?mpg
 
-####################
-#  R4DS EXERCISES  #
-####################
-
-
-#################
-#  QUESTION #1  #
-#################
-
-#Run ggplot(data = mpg). What do you see?
-
-##This question is 3.2.4.#1 and grows your ability to create the ggplot() canvas
-##(link).
+#' **Question 1**: Run ggplot(data = mpg). What do you see?  
 
 ggplot(data=mpg)
 
-##If you don't add anything else, all you see is a gray screen. It doesn't have any plots which would be there.
+#' If you don't add anything else, all you see is a gray screen. It doesn't 
+#' have any plots which would be there.
 
 
-#################
-#  QUESTION #2  #
-#################
+#' **Question 2**: Make a scatterplot of cyl by displ, then make a boxplot to 
+#' visualize the data. What additional information does the boxplot convey? 
 
-#Make a scatterplot of cyl by displ, then make a boxplot to visualize the data. 
-#What additional information does the boxplot convey?  
-
-##Hint: Use as.factor(cyl) when generating the boxplot. This question is based 
-##on 3.2.4.#4 and grows your ability to plot a continuous variable by 
-##categorical variable (link).
 
 mpg %>%
   ggplot() +
@@ -55,52 +29,40 @@ mpg %>%
   ggplot() +
   geom_boxplot(mapping = aes(x=as.factor(cyl),displ))
 
+#' It adds statistical information like median, outliers, quartiles, etc.
 
-
-#################
-#  QUESTION #3  #
-#################
-
-#What happens if you make a scatterplot of class vs drv? Create an alternative 
-#visualization that better conveys the information. 
-
-## Hint: Use geom_bar() with position="fill". Using geom_bar() is covered in
-##3.8. This question is based on 3.2.4.#5 and grows your ability to plot two 
-##categorical variables (link).
+#' **Question 3**: How does
+#' What happens if you make a scatterplot of class vs drv? Create an alternative 
+#' visualization that better conveys the information. 
 
 mpg %>%
   ggplot() +
   geom_point(mapping = aes(x = class , y = drv))
+
+#' It looks really uninterpretable, because there are overlapping points.
 
 mpg %>%
   ggplot() +
   geom_bar(mapping = aes(x = class, fill=drv),position="fill")
 
 
+#' **Question 4**:What geom would you use to draw:
+#' A line chart?
+#' geom_line
 
-#################
-#  QUESTION #4  #
-#################
-#What geom would you use to draw:
-#A line chart?
-##geom_line
+#' A boxplot?
+#' geom_boxplot
 
-#A boxplot?
-##geom_boxplot
+#' A histogram?
+#' geom_histogram
 
-# A histogram?
-##geom_histogram
-
-# An area chart?
-##geom_area()
+#' An area chart?
+#' geom_area()
 
 
 
-#################
-#  QUESTION #5  #
-#################
-
-# Will the graphs created by the two code blocks below look different? Why/why not?
+#' **Question 5**:Will the graphs created by the two code blocks below look 
+#' different? Why/why not?
 
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
   geom_point() + 
@@ -110,4 +72,5 @@ ggplot() +
   geom_point(data = mpg, mapping = aes(x = displ, y = hwy)) + 
   geom_smooth(data = mpg, mapping = aes(x = displ, y = hwy))
 
-##Yes, the code displays the same thing, because the data and mapping carry through if it is used as an argument for ggplot. 
+#' Yes, the code displays the same thing, because the data and mapping carry 
+#' through if it is used as an argument for ggplot. 
