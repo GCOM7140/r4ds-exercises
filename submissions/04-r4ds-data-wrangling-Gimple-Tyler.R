b52370d4eaaea603c2caf3cdd44793b5a520962c
Tyler Gimple <tfg2ce@virginia.edu>
2018-04-13 08:45:17 -0400

    Question 1 done

--- submissions/04-r4ds-data-wrangling-Gimple-Tyler.R
+++ submissions/04-r4ds-data-wrangling-Gimple-Tyler.R
@@ -1,1 +1,19 @@
 # Starter file for HW 4
+#Question 1
+library(tidyverse)
+people <- tribble(
+  ~name,             ~key,    ~value,
+  #-----------------|--------|------
+  "Phillip Woods",   "age",       45,
+  "Phillip Woods",   "height",   186,
+  "Phillip Woods",   "age",       50,
+  "Jessica Cordero", "age",       37,
+  "Jessica Cordero", "height",   156
+)
+
+people <- people %>% 
+  group_by(name, key) %>% 
+  mutate(obs = row_number())
+
+people %>% 
+  spread(key, value)
