d49c04533fdae73337e22deee4d7759a4240adc6
skotz1 <37584709+skotz1@users.noreply.github.com>
2018-04-17 22:53:24 -0400

    Add files via upload

---
+++ submissions/01-r4ds-data-wrangling-Kotz-Sam.R
@@ -0,0 +1,58 @@
+#R4DS Ex. 4
+#Sam Kotz
+
+#1
+library(tidyverse)
+library(nycflights13)
+ 
+people %>% 
+  group_by(name, key) %>% 
+  mutate(id = row_number()) %>% 
+  spread(key, value)
+
+#basically spreading people doesn't work b/c one item (age) has multiple values, confusing R
+
+#2
+preg %>% 
+  gather(`male`, `female`, key = "gender", value = "count")
+
+#you'll have to gather it as shown above
+
+#3
+tibble(x = c("a,b,c", "d,e,f,g", "h,i,j")) %>% 
+  separate(x, c("one", "two", "three"), extra = 'merge')
+
+#basically (if i understand correctly) the second item has multiple values
+
+#4 
+#Remove tells R whether it should remove the original columns
+
+#5
+who %>%
+  gather(code, value, new_sp_m014:newrel_f65, na.rm = TRUE) %>% 
+  mutate(code = gsub("newrel", "new_rel", code)) %>%
+  separate(code, c("new", "var", "sexage")) %>% 
+  select(-new, -iso2, -iso3) %>% 
+  separate(sexage, c("sex", "age"), sep = 1) %>%
+  group_by(country, year) %>%
+  summarise(cases = sum(value)) %>%
+  filter(country %in% c('China', 'India', 'Bangladesh')) %>% 
+  ggplot(aes(x = year, y = cases, colour = country)) + 
+  geom_point() + 
+  geom_line()
+
+#seems like there are some crazy outliers especially in Bangladesh
+
+#6
+flights %>%
+  inner_join(weather, by = c('origin', 'year', 'month', 'day', 'hour')) %>%
+  mutate(
+    wind_gust_binned   = cut(wind_gust, 
+                             breaks = seq(0, max(wind_gust, na.rm = TRUE), 5))
+  ) %>%
+  group_by(wind_gust_binned) %>%
+  summarize(dep_delay = mean(dep_delay, na.rm = TRUE)) %>%
+  ggplot(aes(x = wind_gust_binned, y = dep_delay)) +
+  geom_col()
+
+#about 35mph
