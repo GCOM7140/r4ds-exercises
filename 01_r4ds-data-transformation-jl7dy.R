633ace2b5809c19bc6c7439efd01ba6119a30426
JustinLago <j.lago23@gmail.com>
2019-03-19 15:06:10 -0400

    Push first question - Justin

---
+++ 01_r4ds-data-transformation-jl7dy.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(nycflights13)
+
+
+# How many flights flew into LAX?
+
+flights %>%
+    filter(dest == "LAX") %>%
+    nrow()
+
+# How many flights flew out of LAX?
+
+flights %>%
+  filter(origin == "LAX") %>%
+  nrow()
