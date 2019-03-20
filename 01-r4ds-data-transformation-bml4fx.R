6cee26eaf8305f91e4f8954618f1b36b0eb6a34b
Brian Lenert <bml4fx@virginia.edu>
2019-03-20 14:47:45 -0400

    r4ds data transformation exercises

--- 01-r4ds-data-transformation-bml4fx.R
+++ 01-r4ds-data-transformation-bml4fx.R
@@ -1,25 +1,25 @@
 library(tidyverse)
 library(nycflights13)
 
-#1
+#1a
 # How many flights flew into LAX?
 flights %>% 
   filter(dest == "LAX") %>% 
   nrow()
 
-#2
+#1b
 # How many flights flew out of LAX?
 flights %>% 
   filter(origin == "LAX") %>% 
   nrow()
 
-#3
+#1c
 # How many flights were longer than or equal to 2,000 miles in distance?
 flights %>% 
   filter(distance >= 2000) %>% 
   nrow()
 
-#4
+#1d
 # How many flights were destined for airports in the Los Angeles area (LAX, ONT, 
 # SNA, PSP, SBD, BUR, or LGB), but did not originate out of JFK?
 flights %>% 
@@ -28,3 +28,24 @@ flights %>%
     origin != "JFK"
   ) %>% 
   nrow()
+
+#2
+flights %>% 
+  filter(!is.na(dep_time), is.na(arr_time)) %>% 
+  nrow()
+
+#3
+flights %>% 
+  arrange(desc(is.na(arr_time)))
+
+#4
+select(flights, contains("TIME", ignore.case = TRUE))
+
+#5
+flights %>%
+  filter(distance >= 2000, arr_delay > 0) %>%
+  group_by(dest) %>%
+  summarize(arr_delay_mins = sum(arr_delay)) %>%
+  mutate(arr_delay_pct_of_total = arr_delay_mins / sum(arr_delay_mins)) %>%
+  arrange(desc(arr_delay_pct_of_total)) %>% 
+  head(3)
