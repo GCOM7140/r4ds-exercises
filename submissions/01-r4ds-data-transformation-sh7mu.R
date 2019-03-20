3ad6f49720829a69d6a04d28904267fa9e78b6ec
samirhusain96 <sh7mu@virginia.edu>
2019-03-20 15:39:58 -0400

    Still in progress

--- submissions/01-r4ds-data-transformation-sh7mu.R
+++ submissions/01-r4ds-data-transformation-sh7mu.R
@@ -1,18 +1,36 @@
 library(tidyverse)
 library(nycflights13)
 
-# How many flights flew into LAX?
+# Q1 - How many flights flew into LAX?
 
 flights %>%
   filter(dest == "LAX") %>%
   nrow()
 
-nrow(filter(flights, dest == "LAX"))
-
-# How many flights flew out of LAX?
+# Q1 - How many flights flew out of LAX?
 
 flights %>%
   filter(origin == "LAX") %>%
   nrow()
 
-##test 1
+# Q1 - How many flights were longer than or equal to 2,000 miles in distance?
+
+flights %>%
+  filter(distance >= 2000) %>%
+  nrow()
+
+# Q1 - How many flights were destined for airports in the Los Angeles area (LAX,
+# ONT, SNA, PSP, SBD, BUR, or LGB), but did not originate out of JFK?
+
+flights %>%
+  filter(
+    dest %in% c("LAX", "ONT", "SNA", "PSP", "SBD", "BUR", "LGB") 
+    & origin != "JFK") %>%
+  nrow()
+
+
+# Q2 - How many flights were "ghost flights"? A "ghost flight" is defined as a flight that departed, but never arrived.
+
+flights %>%
+  filter(arr_time ) %>%
+  nrow()
