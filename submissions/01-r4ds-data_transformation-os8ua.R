06ce39562e84267c95a5fd16390ecad984c66d9c
oliversong1 <oliver.song1@gmail.com>
2019-03-20 13:21:51 -0400

    updated Q4

--- submissions/01-r4ds-data_transformation-os8ua.R
+++ submissions/01-r4ds-data_transformation-os8ua.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(nycflights13)
 
+## Question 1 ##
+# A) How many flights flew into LAX
+
 nrow(filter(flights ,dest == "LAX"))
 
 # OR
@@ -8,3 +11,32 @@ nrow(filter(flights ,dest == "LAX"))
 flights %>%
   filter(dest == "LAX") %>%
   nrow()
+
+# B) How many flights flew out of LAX?
+
+flights %>%
+  filter(origin == "LAX") %>%
+  nrow()
+
+# C) How many flights were longer than or equal to 2,000 miles in distance?
+
+flights %>%
+  filter(distance >= 2000) %>%
+  nrow()
+
+# D) How many flights were destined for airports in the LA area (LAX, ONT, SNA,  PSP, SBD, BUR, or LGB) but did not originate out of JFK
+
+la_airports <- c("LAX", "ONT", "SNA", "PSP", "SBD", "BUR", "LGB")
+
+flights %>%
+  filter(origin != "JFK", dest %in% la_airports) %>%
+  nrow()
+
+## Question 2 - How many flights were ghost flights?
+
+flights %>%
+  filter(is.na(arr_time), !is.na(dep_time)) %>%
+  nrow()
+str(flights)
+
+## Question 3 - How does arrange() treat missing values, and how could you sort all of the rows with a
