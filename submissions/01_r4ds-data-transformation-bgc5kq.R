7947856f94997f88e25788f888b0191aa7964f85
bgcorbett95 <48722862+bgcorbett95@users.noreply.github.com>
2019-03-21 11:48:12 -0400

    Add files via upload

--- submissions/01_r4ds-data-transformation-bgc5kq.R
+++ submissions/01_r4ds-data-transformation-bgc5kq.R
@@ -1,62 +1,46 @@
-install.packages("completejourney")
-library(completejourney)
 library(tidyverse)
+library(nycflights13)
 
-#Question 1
-transactions <- transactions %>%
-    mutate(
-      retail_disc       = abs(retail_disc),
-      coupon_disc       = abs(coupon_disc),
-      coupon_match_disc = abs(coupon_match_disc)
-    )
-head(transactions)
+#Question 1a
+flights%>%
+    filter(dest=="LAX") %>%
+    nrow()
+
+#Question 1b
+flights %>%
+    filter(origin=="LAX") %>%
+    nrow()
+
+#Question 1c
+flights %>%
+    filter(distance >= 2000) %>%
+    nrow()
+
+#Question 1d
+flights %>%
+    filter(
+      dest %in% c("LAX", "ONT", "SNA", "PSP", "SBD", "BUR", "LGB"), origin != "JFK"
+    ) %>%
+  nrow()
 
 #Question 2
-(transactions <- transactions %>%
-    mutate(
-      regular_price = (sales_value + retail_disc + coupon_match_disc) / quantity,
-      loyalty_price = (sales_value + coupon_match_disc) / quantity, 
-      coupon_price = (sales_value - coupon_disc) / quantity
-    ))
-
+flights %>%
+    filter(!is.na(dep_time), is.na(arr_time)) %>%
+    nrow()
 
 #Question 3
-transactions %>%
-    filter(regular_price <=1) %>%
-    select(product_id) %>%
-    n_distinct()
+flights %>%
+    arrange(desc(is.na(arr_time)))
 
-transactions %>%
-    filter(loyalty_price <=1) %>%
-    select(product_id) %>%
-    n_distinct()
-
-transactions %>%
-    filter(coupon_price <=1) %>%
-    select(product_id) %>%
-    n_distinct()
-
-#Question 4 
-transactions %>%
-    group_by(basket_id) %>%
-    summarize(basket_value = sum(sales_value)) %>%
-    ungroup %>%
-    summarize(proportion_over_10 = round(mean(basket_value > 10) * 100, 0))
+#Question 4
+select(flights, contains("TIME", ignore.case=TRUE))
 
 #Question 5
-transactions %>%
-  filter(
-    is.finite(regular_price), 
-    is.finite(loyalty_price), 
-    regular_price > 0
-  ) %>%
-  mutate(
-    pct_loyalty_disc     = 1 - (loyalty_price / regular_price)
-  ) %>%
-  group_by(store_id) %>%
-  summarize(
-    total_sales_value    = sum(sales_value), 
-    avg_pct_loyalty_disc = mean(pct_loyalty_disc)
-  ) %>%
-  filter(total_sales_value > 10000) %>%
-  arrange(desc(avg_pct_loyalty_disc))
+flights %>%
+    filter(distance>= 2000, arr_delay >0) %>%
+  group_by(dest) %>%
+  summarize(arr_delay_mins = sum(arr_delay)) %>%
+  mutate(arr_delay_pct_of_total = arr_delay_mins / sum(arr_delay_mins)) %>%
+  arrange(desc(arr_delay_pct_of_total)) %>%
+  head(3)
+  
