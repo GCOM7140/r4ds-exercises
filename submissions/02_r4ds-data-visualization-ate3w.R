26e57c9c144897c512e01476508a06d69eeab4db
Alden Ewing <ate3w@gmail.com>
2019-03-26 11:54:58 -0400

    Add HW2

--- submissions/02_r4ds-data-visualization-ate3w.R
+++ submissions/02_r4ds-data-visualization-ate3w.R
@@ -1,23 +1,31 @@
-# Data Visualization Exercises
+# Data Visualization
 
 # Question 1
 ggplot(data = mpg)
 
+# An empty graph
+
 # Question 2
 ggplot(data = mpg, aes(x = cyl, y = displ)) + 
   geom_point()
+ggplot(data = mpg, aes(x = as.factor(cyl), y = displ)) + 
+  geom_boxplot()
+
+# Shows the same data but boxplots offers a lot more information
 
 # Question 3
-ggplot(mpg, aes(x = class, y = drv)) +
+ggplot(data = mpg, aes(x = class, y = drv)) +
   geom_point()
 ggplot(mpg) +
   geom_bar(mapping = aes(x = class, fill = drv), position = "fill")
 
+# The first graph gives no actual context
+
 # Question 4
-# - geom_line()
-# - geom_boxplot()
-# - geom_histogram()
-# - geom_area() or geom_bar()
+# geom_line()
+# geom_boxplot()
+# geom_histogram()
+# geom_area() or geom_bar()
 
 # Question 5
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
@@ -26,4 +34,5 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
 ggplot() + 
   geom_point(data = mpg, mapping = aes(x = displ, y = hwy)) + 
   geom_smooth(data = mpg, mapping = aes(x = displ, y = hwy))
-#They are the same but the first one is much easier to produce
+
+# These graphs are the same
