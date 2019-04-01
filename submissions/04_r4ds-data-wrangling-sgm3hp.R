fc27d10cf8527236690124bc2d6e9dea05703d13
GraceMays <sgm3hp@virginia.edu>
2019-04-01 13:50:20 -0400

    Completed Questions

---
+++ submissions/04_r4ds-data-wrangling-sgm3hp.R
@@ -0,0 +1,98 @@
+library(nycflights13)
+library(tidyverse)
+
+
+#Question 1: The player tibble below is meant to record De’Andre Hunter and Kyle Guy’s dates of birth and heights. Why can’t you tidy it up with the spread() function? How could you add a new column to player to fix the problem?
+
+player <- tribble(
+  ~name,             ~key,     ~value,
+  #----------------/---------/-----------------------
+  "De'Andre Hunter",    "dob", "December 2, 1997",
+  "De'Andre Hunter", "height", 201,
+  "De'Andre Hunter",    "dob", "August 11, 1997",
+  "Kyle Guy",           "dob", "August 11, 1997",
+  "Kyle Guy",        "height", 191
+)
+
+#you can't use the spread() function because there are two birth dates for De'Andre. Including a filter to just keep one variable can help with this error.
+
+player %>% 
+  group_by(name, key) %>% 
+  mutate(keep = row_number()) %>% 
+  spread(key, value) %>% 
+  filter(keep == 1)
+
+#Question 2: Do you need to gather or spread preg (see below) to tidy it? What variables does preg include?
+
+preg <- tribble(
+  ~pregnant, ~male, ~female,
+  #--------/------/---------
+  "yes",        NA,      10,
+  "no",         20,      12
+)
+
+#need to gather because male and female are variables so they shouldn't be a column title
+
+preg %>%
+  gather(`male`, `female`, key = "gender", value = "count")
+
+
+
+
+#Question 3: How would you explain the warning message below in layman’s terms to someone who couldn’t figure out what it means? Suppose they want to make sure that every piece of the tibble makes its way into the result of the separate() function call. What could they do to remedy the situation?
+
+tibble(x = c("a,b,c", "d,e,f,g", "h,i,j")) %>% 
+  separate(x, c("one", "two", "three"))
+
+## Warning: Expected 3 pieces. Additional pieces discarded in 1 rows [2].
+
+## # A tibble: 3 x 3
+##   one   two   three
+##   <chr> <chr> <chr>
+## 1 a     b     c    
+## 2 d     e     f    
+## 3 h     i     j
+
+#the function was expecting there to be three letters in the set but in second row there were four letters so one was eliminated. This could be remedied by using extra = "merge" in the code.
+
+#Question 4: Both separate() and unite() have a remove argument. What does it do? When does setting it to FALSE make sense?
+
+#The remove argument determines whether to removes the input column from the ouput data frame. If trying to determine if an error is made then keeping the input column (setting the argument to FALSE) makes sense 
+
+
+
+#Question 5: Using the who dataset, calculate the total number of cases of TB per year for China, India, and Bangladesh, then plot these statistics over time. What country-year statistics, if any, surprise you? You can tidy the who dataset with:
+
+who %>%
+  gather(code, value, new_sp_m014:newrel_f65, na.rm = TRUE) %>% 
+  mutate(code = gsub("newrel", "new_rel", code)) %>%
+  separate(code, c("new", "var", "sexage")) %>% 
+  select(-new, -iso2, -iso3) %>% 
+  separate(sexage, c("sex", "age"), sep = 1)
+
+who %>%
+  gather(code, value, new_sp_m014:newrel_f65, na.rm = TRUE) %>% 
+  mutate(code = gsub("newrel", "new_rel", code)) %>%
+  separate(code, c("new", "var", "sexage")) %>% 
+  select(-new, -iso2, -iso3) %>% 
+  separate(sexage, c("sex", "age"), sep = 1) %>%
+  group_by(country, year) %>%
+  summarise(cases = sum(value)) %>%
+  filter(country %in% c('China', 'India', 'Bangladesh')) %>% 
+  ggplot(aes(x = year, y = cases, colour = country)) + 
+  geom_point() + 
+  geom_line()
+
+
+
+#Question 6: At what wind speeds are departure delays out of NYC the longest on average?
+
+flights %>%
+  inner_join(weather, by = c("origin", "year", "month", "day", "hour")) %>%
+  mutate(
+    wind_speed_binned = cut_width(wind_speed, width = 5, center = 2.5)
+  ) %>%
+  group_by(wind_speed_binned) %>%
+  summarize(dep_delay = mean(dep_delay, na.rm = TRUE)) 
+  
+
